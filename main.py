6ad886195182d0d00862c8c08e4d6a9512c48a61
njcalvo <njcalvo@gmail.com>
2021-03-09 15:16:15 -0300

    Add mergeSort function
    - Add main.py with example

---
+++ main.py
@@ -0,0 +1,6 @@
+from mergeSort import mergeSort
+
+l1 = ['Cristiano', 'Drogba', 'Kaka', 'Leo', 'Lukaku']
+l2 = ['Armani', 'Bebeto', 'Mascherano', 'Rojo']
+
+print(mergeSort(l1, l2))
---
+++ mergeSort.py
@@ -0,0 +1,5 @@
+def mergeSort(l1, l2):
+    l = l1 + l2
+    l.sort()
+
+    return l
